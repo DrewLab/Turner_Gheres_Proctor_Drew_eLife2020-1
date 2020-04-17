edeb025d858e9f60df59011e0a3b3552c2b0a24d
KL-Turner <klt8@psu.edu>
2020-04-16 22:31:14 -0400

    .

--- UntitledD.m
+++
@@ -1,9 +0,0 @@
-animalIDs = {'T121','T122'};
-rootFolder = cd;
-for aa = 1:length(animalIDs)
-    animalID = animalIDs{1,aa};
-    animalDir = [rootFolder '\' animalID '\Bilateral Imaging\'];
-    cd(animalDir)
-    StageTwoProcessing2_IOS_Manuscript2020()
-    cd(rootFolder)
-end
